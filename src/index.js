28d74d856e9b73f4a8ba46bb1635c1a6e7844a72
majinquan <286841868@qq.com>
2020-12-03 17:53:42 +0800

    webpack1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * 打包 开发环境 webpack ./src/index.js -o ./build/built.js --mode=development
+ * 打包 生产环境 webpack ./src/index.js -o ./build/built.js --mode=production
+ */
+function add(x, y) {
+  return x + y
+}
+add();
